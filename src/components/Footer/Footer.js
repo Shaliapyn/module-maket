37811ae281bae32d55467e0d3fe029310f7c753c
Ihor Shalapa <igorshalapa@gmail.com>
2022-04-21 17:30:30 +0300

    correct footer input

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,78 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./Footer.module.scss"
+import styles from "./Footer.module.scss";
 
-import { GoLocation } from "react-icons/go"
-import { BsEnvelope, BsTelephone } from "react-icons/bs"
-import { IconContext } from 'react-icons'
+import { GoLocation } from "react-icons/go";
+import { BsEnvelope, BsTelephone } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 const Footer = () => {
   return (
     <footer className={styles.footerBlock}>
-        <div className={styles.twoBlocks}>
+      <ul className={styles.absoluteNav}>
+         <li>Preise</li>
+         <li>Preferenzen</li>
+         <li>Kontakt</li>
+       </ul>
+      <div className={styles.twoBlocks}>
         <div className={styles.leftBlock}>
-            <ul className={styles.contacts}>
-            <IconContext.Provider value={{size: "1.8em"}}>
-                <li><GoLocation className={styles.icon} />{"Poststr, 45, 44890 Hamburg"}</li>
-                <li><BsEnvelope className={styles.icon}/>{"HELLO@TRATATA.DE"}</li>
-                <li><BsTelephone className={styles.icon}/>{"040 380-33- 44"}</li>
+          <ul className={styles.contacts}>
+            <IconContext.Provider value={{ size: "1.8em" }}>
+              <li>
+                <GoLocation className={styles.icon} />
+                {"Poststr, 45, 44890 Hamburg"}
+              </li>
+              <li>
+                <BsEnvelope className={styles.icon} />
+                {"HELLO@TRATATA.DE"}
+              </li>
+              <li>
+                <BsTelephone className={styles.icon} />
+                {"040 380-33- 44"}
+              </li>
             </IconContext.Provider>
-            </ul>
+          </ul>
         </div>
-        <div className={styles.rightBlock}></div>
-        <div className={styles.footerLast}>© 2019  Genius Web Agentur. Alle Rechte vorbehalten</div>
+        <div className={styles.rightBlock}>
+          <div className={styles.flexContainer}>
+            <h2 className={styles.titleForm}>Schreiben Sie uns!</h2>
+            <form className={styles.form}>
+              <input
+                className={styles.firstInp}
+                type="text"
+                placeholder="Firma/organisation"
+              />
+              <div className={styles.twoInpRow}>
+                <input
+                  className={styles.secondInp}
+                  type="email"
+                  placeholder="E-mail"
+                  name="email"
+                />
+                <input
+                  className={styles.thirdInp}
+                  type="tel"
+                  placeholder="Telefon"
+                />               
+              </div>
+              <textarea
+                className={styles.message}
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Was ist ihr anliegen?"
+              ></textarea>
+            </form>
+            <button className={styles.formBtn}>Senden</button>
+          </div>
         </div>
-        
+      </div>
+      <div className={styles.footerLast}>
+        © 2019 Genius Web Agentur. Alle Rechte vorbehalten
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
